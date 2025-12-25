502033e056670a652b8f2418c8a7e8feecd449b1
madoka <uki.civilization@gmail.com>
2025-12-25 16:55:48 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,11 @@
+import streamlit as st
+
+st.set_page_config(page_title="My App", layout="wide")
+
+st.title("メインWebアプリ")
+st.write("これは app.py がメインです")
+
+name = st.text_input("名前を入力")
+if name:
+    st.success(f"こんにちは、{name} さん")
 
